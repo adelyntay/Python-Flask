2207156199242a47c17e0178402f5f6fa6d276ac
ade-t <ad_elyn@hotmail.com>
2023-04-17 23:01:32 +0800

    install CORS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os 
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
@@ -6,6 +7,7 @@ from markupsafe import escape
 from pymongo import MongoClient
 
 app = Flask(__name__)
+CORS(app)
 load_dotenv()
 app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
 mongo = PyMongo(app)
